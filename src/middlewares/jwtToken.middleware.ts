21d7e12e2a2aeded8f86218adb2fde227fcf9167
Krishna Chaudhary <krishnachaudhary8452@gmail.com>
2024-05-27 00:17:12 +0545

    fix: add validation in the middleware

--- src/middlewares/jwtToken.middleware.ts
+++ src/middlewares/jwtToken.middleware.ts
@@ -1,17 +1,14 @@
-import jwt, { Secret, JwtPayload } from "jsonwebtoken";
+import jwt, { Secret, JwtPayload, JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { Users } from "src/helpers";
 import * as dotenv from "dotenv";
 import path from "path";
-import { BadRequestError } from "src/common/utils";
 dotenv.config({ path: path.join(__dirname, ".env") });
-export const SECRET_KEY: Secret = `${process.env.JWT_SECRET_KEY}`; 
+export const SECRET_KEY: Secret = `${process.env.JWT_SECRET_ACCESSTOKEN_KEY}`; 
 export const jwtTokenMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
   try {
     const token = req.header("Authorization")?.replace("Bearer ", "");
-
     if (!token)  throw new Error("Token missing");
-
     const decoded = jwt.verify(token, SECRET_KEY) as JwtPayload;
     const id: any = decoded.id;
     const userProfile = await Users.findUnique({
@@ -22,13 +19,15 @@ export const jwtTokenMiddleware = async (req: Request, res: Response, next: Next
 
     if(userProfile?.deleted) throw new Error("User is deleted")
     next();
-
-  } catch (error ) {
-    if ( error instanceof Error && error.message === "Token missing") {
-      return res.json("Token missing");
-    }else if(error instanceof Error && error.message === "User is deleted"){
-      return res.json("User is deleted")
-    }
-     throw new BadRequestError("Token is Expired")
+  } catch(error ) {
+    if (error instanceof JsonWebTokenError){
+      return res.json("Invalid token.")
+    } else if (error instanceof TokenExpiredError){
+      return res.json("Token expired.")
+    } else if ( error instanceof Error && error.message === "Token missing.") {
+      return res.json("Token missing.");
+    } else if(error instanceof Error && error.message === "User is deleted."){
+      return res.json("User is deleted.")
+    } else return res.status(500).json("Internal server error.")
   }
 };
