5f72ce9f973014b0fc9cce0193b1daff62e9363c
Krishna Chaudhary <krishnachaudhary8452@gmail.com>
2024-05-27 00:15:30 +0545

    feat: add env variables in config file

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -4,7 +4,7 @@ export default {
   appConfig: {
     PORT: env.PORT,
     APP_URL: env.APP_URL,
-    NODE_ENV: env.NODE_ENV,
+    NODE_ENV: env.NODE_ENV, 
     APP_NAME: env.APP_NAME,
 
   },
@@ -12,7 +12,12 @@ export default {
     DATABASE_URL: env.DATABASE_URL
   },
   jwtConfig:{
-    JWT_SECRET_KEY:env.JWT_SECRET_KEY
-
+    JWT_SECRET_ACCESSTOKEN_KEY:env.JWT_SECRET_ACCESSTOKEN_KEY,
+    JWT_SECRET_REFRESHTOKEN_KEY:env.JWT_SECRET_REFRESHTOKEN_KEY,
+    JWT_SECRET_RESET_PIN_KEY:env.JWT_SECRET_RESET_PIN_KEY
+  },
+  mailConfig:{
+    MAILING_USER:env.MAILING_USER,
+    MAILING_PASS:env.MAILING_PASS,
   }
 };
